f0af8bc6a5fe101222fe6b5194b906fb4228be4c
Naz <nchinazaeze@gmail.com>
2017-02-19 09:43:04 +0000

    Added Time Class and functionality to parse input time string

---
+++ src/main/java/io/github/nazcompile/berlinclock/Time.java
@@ -0,0 +1,52 @@
+package io.github.nazcompile.berlinclock;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Time {
+		
+	private int hour;
+	private int minutes;
+	private int seconds;
+	
+	private Time() { }
+
+	public int getHour() {
+		return hour;
+	}
+
+	public int getMinutes() {
+		return minutes;
+	}
+
+	public int getSeconds() {
+		return seconds;
+	}
+	
+	public static Time parseTime(String timeString) {
+		Time time = new Time();
+		String timePattern = "HH:mm:ss";
+		SimpleDateFormat sdf = new SimpleDateFormat(timePattern);
+		sdf.setLenient(false); 
+		
+		try {
+			Date date = sdf.parse(timeString);
+			
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTime(date);
+			
+			time.hour = calendar.get(Calendar.HOUR_OF_DAY);
+			time.minutes = calendar.get(Calendar.MINUTE);
+			time.seconds = calendar.get(Calendar.SECOND);
+
+		} catch (ParseException e) {
+			//log e
+			throw new IllegalArgumentException("Invalid time provided. Cause - " + e.getMessage());
+		}
+		
+		return time;
+	}
+
+}
---
+++ src/test/groovy/io/github/nazcompile/berlinclock/TimeSpec.groovy
@@ -0,0 +1,33 @@
+package io.github.nazcompile.berlinclock
+
+import spock.lang.Specification;
+
+class TimeSpec extends Specification {
+	
+	def "Should be able to correctly parse a valid input time"() {
+		when:
+			Time time = Time.parseTime(timeValue)
+			
+		then:
+			expectedHour == time.getHour()
+			expectedMins == time.getMinutes()
+			expectedSecs == time.getSeconds()
+			
+		where:
+			timeValue	|	expectedHour	|	expectedMins	|	expectedSecs
+			'13:27:45'	|		13			|		27			|		45
+			'02:15:36'	|		2			|		15			|		36
+			'23:30:03'	|		23			|		30			|		3
+			'00:00:14'	|		0			|		0			|		14
+	}
+	
+	def "Should throw an Illegal argument exception if an invalid time is provided"() {
+		when:
+			Time.parseTime(invalidTime)
+		then:
+			thrown IllegalArgumentException
+		where:
+			invalidTime << ['24:02:13', '12:30pm']
+	}
+
+}
